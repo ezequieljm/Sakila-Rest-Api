d967a60a79df6d4b73c99fafa7eb7575ee7ea946
ezequieljm <ezequieljm020@gmail.com>
2022-12-29 17:55:35 -0300

    CRUD for customer finished. Now we are going to purge

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -18,11 +18,10 @@ export function createANewCustomer(req: Request, res: Response, next: NextFuncti
         createDate,
         lastUpdate,
     ])
-        // .then((result) => res.status(200).json({ message: "Registered customer", result }))
-        .then((result) => console.log(result))
-        .catch((error: Error) => res.status(500).json({ message: error.message, stack: error.stack }));
-
-    pool.end();
+        .then((result) =>
+            res.status(200).json({ message: "Registered customer", insertedId: parseInt(result.insertId) })
+        )
+        .catch((error: Error) => res.status(500).json({ message: error.message }));
 }
 
 // Read all customers
@@ -34,8 +33,45 @@ export function readAllCustomer(req: Request, res: Response, next: NextFunction)
                 : res.status(200).send(rows)
         )
         .catch((err) => res.status(err.status || 500).json({ message: err.message }));
+}
 
-    pool.end();
+//Update customer by id
+export function updateCustomerById(req: Request, res: Response, next: NextFunction) {
+    const customerId: string = req.params.id;
+    const { storeId, firstname, lastname, email, addressId, active, createDate, lastUpdate } = req.body;
+    const updateCustomerQuery: string = `
+        UPDATE customer
+        SET store_id = IFNULL(?,store_id),
+            first_name = IFNULL(?,first_name),
+            last_name = IFNULL(?,last_name),
+            email = IFNULL(?,email),
+            address_id = IFNULL(?,address_id),
+            active = IFNULL(?,active),
+            create_date = IFNULL(?,create_date),
+            last_update = IFNULL(?,last_update)
+        WHERE customer_id = ?
+        
+    `; // do not forget the where clause, this is very important !!!
+
+    pool.query(updateCustomerQuery, [
+        storeId,
+        firstname,
+        lastname,
+        email,
+        addressId,
+        active,
+        createDate,
+        lastUpdate,
+        customerId,
+    ])
+        .then((result) =>
+            result.affectedRows === 0
+                ? res.status(500).json({ message: "Not is possible update the customer. Not found" })
+                : res.status(200).json({ message: "Updated customer" })
+        )
+        .catch((error: Error) =>
+            res.status(500).json({ message: "An error has occurred", error: error.message })
+        );
 }
 
 // Delete customer by id
@@ -46,9 +82,10 @@ export function deleteCustomerById(req: Request, res: Response, next: NextFuncti
         WHERE customer_id = ?
     `;
     pool.query(deleteCustomerQuery, [customerId])
-        .then(console.log)
-        .then(() => res.status(200).json({ message: "Deleted customer" }))
+        .then((result) =>
+            result.affectedRows === 0
+                ? res.status(500).json({ message: "Customer not found" })
+                : res.status(200).json({ message: "Deleted customer" })
+        )
         .catch((error: Error) => res.status(500).json({ message: error.message }));
-
-    pool.end();
 }
--- src/routes/customer.routes.ts
+++ src/routes/customer.routes.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
-import { createANewCustomer, deleteCustomerById, readAllCustomer } from "../controllers/customer.controller";
+import {
+    createANewCustomer,
+    deleteCustomerById,
+    readAllCustomer,
+    updateCustomerById,
+} from "../controllers/customer.controller";
 
 const router: Router = Router();
 
 router.get("/read_all", readAllCustomer);
+
 router.post("/register", createANewCustomer);
+
+router.put("/update/:id", updateCustomerById);
+
 router.delete("/delete/:id", deleteCustomerById);
 
 export default router;
